26050fca0a53f574b52ac4de857232884ce6236d
Michael Cooper <cooper@w3.org>
2021-02-09 18:15:47 -0500

    making it more api like

--- wcag3/api/index.php
+++ wcag3/api/index.php
@@ -1,8 +1,12 @@
-<?php 
-	require "queries.phi";
-	
-	header('Content-type: application/json');
-	print (json_encode(dataToArray()));
-
-
-?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>WCAG 3 API</title>
+	</head>
+	<body>
+		<h1>WCAG 3 API</h1>
+		<ul>
+			<li><a href="types">List of guideline types</a></li>
+		</ul>
+	</body>
+</html>
---
+++ wcag3/api/types.php
@@ -0,0 +1,5 @@
+<?php
+	$search = "types";
+	include "execute.phi";
+	
+?>
